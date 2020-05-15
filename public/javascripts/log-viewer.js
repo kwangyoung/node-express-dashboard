eafbd7652738b3d6f65e33b1d9eff1080b312686
Kwangyoung <kwangyoung.kim@gmail.com>
2020-05-15 13:39:31 +0900

    Completed module

---
+++ public/javascripts/log-viewer.js
@@ -0,0 +1,10 @@
+const connection = new WebSocket("ws://localhost:3000");
+const logWindow = document.querySelector('#log-window');
+
+connection.onopen = function () {
+  connection.send("Hello from the client!");
+};
+
+connection.onmessage = function (event) {
+  logWindow.innerHTML = event.data;
+};
